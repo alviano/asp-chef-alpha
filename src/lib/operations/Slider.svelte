f6adaefea19cbb35d52fd2152b2b6e3d86fe4fc2
Mario Alviano <mario.alviano@gmail.com>
2023-03-20 21:00:06 +0100

    Slider to set an integer constant (in a unary predicate)

--- src/lib/operations/Slider.svelte
+++ src/lib/operations/Slider.svelte
@@ -65,8 +65,8 @@
             The <strong>{operation}</strong> operation extends input models with a new fact storing the given integer.
         </p>
         <p>
-            The range of selectable values can be specified by means of constants or by a unary predicate.
-            If unary predicates are used, the extreme values among all models in input are used.
+            The range of selectable values can be specified by means of constants or by (the first argument of) a predicate.
+            If predicates are used, the extreme values among all models in input are used.
         </p>
     </div>
     <InputGroup>
