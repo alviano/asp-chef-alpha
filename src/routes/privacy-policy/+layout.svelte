24c542c80b9a6f59e54de1a587f1280490006ffa
Mario Alviano <mario.alviano@gmail.com>
2023-04-17 11:57:43 +0200

    add privacy statement

---
+++ src/routes/privacy-policy/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import {Col, Row} from "sveltestrap";
+</script>
+
+<Row class="vw-100 vh-100" style="overflow: hidden;">
+  <Col class="vw-100 vh-100" style="overflow-y: scroll;">
+    <slot />
+  </Col>
+</Row>
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,25 @@
 import adapter from '@sveltejs/adapter-auto';
+import { mdsvex } from "mdsvex";
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+      		extensions: ['.md']
+		})
+	],
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+		  	fallback: '/',
+		}),
 	},
+
+    extensions: ['.svelte', '.md'],
 };
 
 export default config;
